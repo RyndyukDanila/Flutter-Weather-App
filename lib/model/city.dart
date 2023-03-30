5dcab0cfcf1181131c86fa705efb4e8e73a2b22e
RyndyukDanila <ryndyukdanila@gmail.com>
2023-03-30 08:25:32 +0600

    Draggable list of cities
    Draggable list of cities with ReorderableListView

---
+++ lib/model/city.dart
@@ -0,0 +1,10 @@
+class City {
+  final String name;
+
+  City({required this.name});
+
+  @override
+  String toString() {
+    return name;
+  }
+}
--- lib/view/pages/locations_page.dart
+++ lib/view/pages/locations_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../model/city.dart';
+
 class LocationsPage extends StatefulWidget {
   const LocationsPage({super.key});
 
@@ -8,12 +10,45 @@ class LocationsPage extends StatefulWidget {
 }
 
 class _LocationsPageState extends State<LocationsPage> {
-  int _counter = 0;
+  final TextEditingController _textFieldController = TextEditingController();
+  final List<City> _cities = <City>[];
 
-  void _incrementCounter() {
+  void _addCityItem(String name) {
     setState(() {
-      _counter++;
+      _cities.add(City(name: name));
     });
+    _textFieldController.clear();
+  }
+
+  void _handleCityDelete(City city) {
+    setState(() {
+      _cities.remove(city);
+    });
+  }
+
+  Future<void> _displayDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add a new city'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: const InputDecoration(hintText: 'Type your new city'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Add'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _addCityItem(_textFieldController.text);
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -21,22 +56,36 @@ class _LocationsPageState extends State<LocationsPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Locations Page"),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.add_rounded),
+              onPressed: () {
+                _displayDialog();
+              }),
+        ],
       ),
-      body: Container(
-        child: ListView.separated(
-          itemCount: _counter,
-          itemBuilder: (context, index) => ListTile(
-            title: Text('$index'),
-          ),
-          separatorBuilder: (BuildContext context, int index) {
-            return Divider();
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: ReorderableListView(
+        children: <Widget>[
+          for (int index = 0; index < _cities.length; index += 1)
+            ListTile(
+              key: Key('$index'),
+              title: Text('${_cities[index]}'),
+              leading: Icon(Icons.drag_handle_rounded),
+              trailing: IconButton(
+                icon: Icon(Icons.close_rounded),
+                onPressed: () => _handleCityDelete(_cities[index]),
+              ),
+            ),
+        ],
+        onReorder: (int oldIndex, int newIndex) {
+          setState(() {
+            if (oldIndex < newIndex) {
+              newIndex -= 1;
+            }
+            final City item = _cities.removeAt(oldIndex);
+            _cities.insert(newIndex, item);
+          });
+        },
       ),
     );
   }
